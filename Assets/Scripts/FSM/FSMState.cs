d73ebba0b8e83b0ca8a08b98dd941a1669c16869
xEoDx <victor.alcazar.alcazar@gmail.com>
2019-11-29 19:23:48 +0100

    Add State Machine

---
+++ Assets/Scripts/FSM/FSMState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class FSMState
+{
+    protected GameObject gameObject;
+
+    protected FSMState(GameObject gameObject)
+    {
+        this.gameObject = gameObject;
+    }
+
+    public abstract void Init();
+    public abstract Type Execute();
+    public abstract void Exit();
+}
---
+++ Assets/Scripts/FSM/StateMachine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace FSM
+{
+    public class StateMachine : MonoBehaviour
+    {
+        private IDictionary<Type, FSMState> _states;
+        private FSMState _currentState;
+
+        public event Action<FSMState> OnStateChanged;
+
+        public void SetStates(Dictionary<Type, FSMState> states)
+        {
+            _states = states;
+        }
+
+        private void Update()
+        {
+            if (_currentState == null && _states.Count > 0)
+            {
+                var initialState = _states.Keys.First();
+                ChangeState(initialState);
+            }
+
+            var nextState = _currentState?.Execute();
+
+            if (nextState != null && nextState != _currentState?.GetType())
+            {
+                ChangeState(nextState);
+            }
+        }
+        
+        private void ChangeState(Type newState)
+        {
+            Assert.IsNotNull(newState);
+            _currentState?.Exit();
+            _currentState = _states[newState];
+            OnStateChanged?.Invoke(_currentState);
+            _currentState.Init();
+            
+        }
+    }
+}
