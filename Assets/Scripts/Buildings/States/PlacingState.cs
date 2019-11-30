486830ed0f2963f54acd64c43fec04f564cc36b1
xEoDx <victor.alcazar.alcazar@gmail.com>
2019-11-30 15:34:43 +0100

    Add new state for Placing a tower

---
+++ Assets/Scripts/Buildings/States/PlacingState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Buildings.States
+{
+    public class PlacingState : FSMState
+    {    
+        
+
+        public PlacingState(Tower tower) : base(tower.gameObject)
+        {
+        }
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
