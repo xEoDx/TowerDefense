3c5c0acbe490b7a99ccc8f2986816145a3b9d7a3
xEoDx <victor.alcazar.alcazar@gmail.com>
2019-11-30 23:22:31 +0100

    Prevent attacking mobs that are blocked by a wall

--- Assets/Scripts/Buildings/States/AttackState.cs
+++ Assets/Scripts/Buildings/States/AttackState.cs
@@ -2,17 +2,21 @@
 using AI;
 using Ammo;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 
 namespace Buildings.States
 {
     public class AttackState : FSMState
     {
+        private readonly int _enemyMask = LayerMask.GetMask("Enemy");
+        private readonly int _obstacleMask = LayerMask.GetMask("Obstacle");
+        private const float MaxStuckTime = 1.5f;
+       
         private Tower _tower;
         private AmmoPool _ammoPool;
         private Enemy _currentTarget;
         private Transform _enemyTransform;
         private float _attackElapsedTime;
+        private float _elapsedStuckTime;
         public AttackState(Tower tower) : base(tower.gameObject)
         {
             _tower = tower;
@@ -21,6 +25,7 @@ namespace Buildings.States
 
         public override void Init()
         {
+            _elapsedStuckTime = 0;
             _attackElapsedTime = 0;
             _currentTarget = _tower.GetCurrentTarget();
             _enemyTransform = _tower.GetCurrentTarget().transform;
@@ -36,17 +41,76 @@ namespace Buildings.States
             }
 
             UpdateRotation();
-            
-            if (_attackElapsedTime > _tower.Attributes.AttackSpeed)
-            {
-                _attackElapsedTime = 0;
-                _ammoPool.Shoot(_enemyTransform.position);
-            }
 
-            _attackElapsedTime += Time.deltaTime;
+            if (IsFacingEnemy(_enemyTransform.position))
+            {
+                if (!IsObstacleInFront())
+                {
+                    _elapsedStuckTime = 0;
+                    if (_attackElapsedTime > _tower.Attributes.AttackSpeed)
+                    {
+                        _attackElapsedTime = 0;
+                        _ammoPool.Shoot(_enemyTransform.position);
+                    }
+        
+                    _attackElapsedTime += Time.deltaTime;
+                }
+                else
+                {
+                    if (_elapsedStuckTime > MaxStuckTime)
+                    {
+                        return typeof(RadarState);
+                    }
+                    _elapsedStuckTime += Time.deltaTime;
+                }
+            }
 
             return typeof(AttackState);
         }
+        
+        private bool IsObstacleInFront()
+        {
+            var direction = _enemyTransform.position - _tower.RotatingElementTransform.position;
+            var hits = Physics.RaycastAll(_tower.RotatingElementTransform.position,
+                direction,
+                _tower.Attributes.Range,
+                _enemyMask | _obstacleMask);
+ 
+            float enemyDistance = float.MaxValue;
+            float obstacleDistance = float.MaxValue;
+            foreach (var hit in hits)
+            {
+                var distance = Vector3.Distance(hit.transform.position, _tower.RotatingElementTransform.transform.position);
+                if (1 << hit.transform.gameObject.layer == _enemyMask )
+                {
+                    if (distance < enemyDistance)
+                    {
+                        enemyDistance = distance;
+                    }
+                }
+                else if (1 << hit.transform.gameObject.layer == _obstacleMask)
+                {
+                    if (distance < obstacleDistance)
+                    {
+                        obstacleDistance = distance;
+                    }
+                }
+            }
+
+            return obstacleDistance < enemyDistance;
+        }
+        
+        private bool IsFacingEnemy(Vector3 enemyPosition)
+        {
+            var heading = enemyPosition - _tower.RotatingElementTransform.position;
+            
+            var dotProduct = Vector3.Dot(heading, _tower.RotatingElementTransform.forward);
+
+            
+
+            return dotProduct < 15;
+        }
+
 
         public override void Exit()
         {}
